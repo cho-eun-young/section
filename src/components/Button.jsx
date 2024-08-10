4573f6e830dc1051e07601d046009d85c99edec3
cho-eun-young <choeunyoung224@gmail.com>
2024-08-10 23:34:47 +0900

    state-bulb

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,19 @@
 // props는 부모 컴포넌트에서 자식 컴포넌트로만 전달할 수 있다.
-const Button = ({text, color, children}) => {
-  return (<button style={{color: color}}>{text} - {color.toUpperCase()}
-  {children}
-  </button>)
+const Button = ({ text, color, children }) => {
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e)
+    console.log(text);
+  }
+
+  return (
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}>{text} - {color.toUpperCase()}
+      {children}
+    </button>
+  )
 }
 
 Button.defaultProps = {
